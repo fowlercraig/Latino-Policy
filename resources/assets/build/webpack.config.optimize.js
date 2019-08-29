42709ca9982e6a097dd18bd38a1801a50acce81d
Craig Fowler <craig@howler.la>
2019-08-29 08:44:46 -0700

    Update webpack.config.optimize.js

--- resources/assets/build/webpack.config.optimize.js
+++ resources/assets/build/webpack.config.optimize.js
@@ -3,8 +3,65 @@
 const { default: ImageminPlugin } = require('imagemin-webpack-plugin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob-all');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 const config = require('./config');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+const purgecssWordpress = {
+  whitelistPatterns: [
+      /^(is-|has-|will-|js-|fs-|feather-|word-|aos-|aspect-)/,
+      /^rtl(-.*)?$/,
+      /^home(-.*)?$/,
+      /^blog(-.*)?$/,
+      /^archive(-.*)?$/,
+      /^date(-.*)?$/,
+      /^error404(-.*)?$/,
+      /^logged-in(-.*)?$/,
+      /^admin-bar(-.*)?$/,
+      /^no-customize-support(-.*)?$/,
+      /^wp-custom-logo(-.*)?$/,
+      /^search(-.*)?$/,
+      /^nav(-.*)?$/,
+      /^wp(-.*)?$/,
+      /^screen(-.*)?$/,
+      /^navigation(-.*)?$/,
+      /^(.*)-template(-.*)?$/,
+      /^(.*)?-?single(-.*)?$/,
+      /^postid-(.*)?$/,
+      /^post-(.*)?$/,
+      /^attachmentid-(.*)?$/,
+      /^attachment(-.*)?$/,
+      /^page(-.*)?$/,
+      /^(post-type-)?archive(-.*)?$/,
+      /^author(-.*)?$/,
+      /^gallery(-.*)?$/,
+      /^category(-.*)?$/,
+      /^tag(-.*)?$/,
+      /^card(-.*)?$/,
+      /^menu(-.*)?$/,
+      /^tags(-.*)?$/,
+      /^tax-(.*)?$/,
+      /^term-(.*)?$/,
+      /^date-(.*)?$/,
+      /^(.*)?-?paged(-.*)?$/,
+      /^says(-.*)?$/,
+      /^depth(-.*)?$/,
+      /^comment(-.*)?$/,
+      /^comments(-.*)?$/,
+      /^children(-.*)?$/,
+      /^crnb(-.*)?$/,
+      /^custom(-.*)?$/,
+      /^custom-background(-.*)?$/,
+      /^port-description(-.*)?$/
+  ]
+};
 
 module.exports = {
   plugins: [
@@ -19,8 +76,22 @@ module.exports = {
           { removeViewBox: false },
         ],
       },
-      plugins: [imageminMozjpeg({ quality: 75 })],
+      plugins: [imageminMozjpeg({ quality: 65 })],
       disable: (config.enabled.watcher),
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync([
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'resources/assets/scripts/**/*.js',
+      ]),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["js", "php"]
+        }
+      ],
+      whitelistPatterns: purgecssWordpress.whitelistPatterns
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
