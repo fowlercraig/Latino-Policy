de24666e6064b0c82259a5cf91f32cb554a76c5e
Craig Fowler <craig@howler.la>
2020-05-21 15:28:43 -0700

    padding mailchimp

--- resources/views/partials/mailchimp.blade.php
+++ resources/views/partials/mailchimp.blade.php
@@ -1,42 +1,43 @@
 <!-- Begin Mailchimp Signup Form -->
 <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-  /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
 <div id="mc_embed_signup">
-<form action="https://ucla.us16.list-manage.com/subscribe/post?u=d021c35e6a05ad46f78ac6f1d&amp;id=1d321a06c6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-  <h2>Subscribe</h2>
+<form 
+  style="padding:0" 
+  action="https://ucla.us16.list-manage.com/subscribe/post?u=d021c35e6a05ad46f78ac6f1d&amp;id=1d321a06c6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<div id="mc_embed_signup_scroll">
 <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
 <div class="mc-field-group">
-  <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
 </label>
-  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 </div>
 <div class="mc-field-group">
-  <label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
 </label>
-  <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
 </div>
 <div class="mc-field-group">
-  <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
 </label>
-  <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
 </div>
 <div class="mc-field-group">
-  <label for="mce-MMERGE3">Organization  <span class="asterisk">*</span>
+<label for="mce-MMERGE3">Organization  <span class="asterisk">*</span>
 </label>
-  <input type="text" value="" name="MMERGE3" class="required" id="mce-MMERGE3">
+<input type="text" value="" name="MMERGE3" class="required" id="mce-MMERGE3">
 </div>
-  <div id="mce-responses" class="clear">
-    <div class="response" id="mce-error-response" style="display:none"></div>
-    <div class="response" id="mce-success-response" style="display:none"></div>
-  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d021c35e6a05ad46f78ac6f1d_1d321a06c6" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
+<div id="mce-responses" class="clear">
+<div class="response" id="mce-error-response" style="display:none"></div>
+<div class="response" id="mce-success-response" style="display:none"></div>
+</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d021c35e6a05ad46f78ac6f1d_1d321a06c6" tabindex="-1" value=""></div>
+<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</div>
 </form>
 </div>
 <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
