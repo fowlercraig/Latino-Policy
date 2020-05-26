f45ae307cd7bac2bd63449172e222d77596be64f
Craig Fowler <craig@howler.la>
2020-05-26 09:35:04 -0700

    newsroom

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,4 +13,21 @@
 
   @include('partials.newsroom.press')
 
+  <div class="relative pt-10 lg:pt-20">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 space-y-10 relative z-10">
+      <div>
+        <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-brand-darker sm:text-4xl sm:leading-10  mb-0">News & Press</h2> 
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-4 gap-8">
+        @while(have_posts()) @php(the_post())
+        @include('partials.modules.article-item')
+        @endwhile
+      </div>
+    </div>
+    <div class="opacity-25 md:w-2/5 absolute top-0 left-0 pointer-events-none z-0 lg:-mt-20">
+      <img class="opacity-50 w-full" src="@asset('images/Pattern.png')" srcset="@asset('images/Pattern.png') 1x, @asset('images/Pattern@2x.png') 2x, @asset('images/Pattern@3x.png') 3x">
+    </div>
+  </div>
+
+
 @endsection
