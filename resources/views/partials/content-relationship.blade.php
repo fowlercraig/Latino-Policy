85052f3b785c65b60f297e0c5b8386cdc82f8122
Craig Fowler <craig@howler.la>
2020-05-26 19:16:11 -0700

    thangs

--- resources/views/partials/content-relationship.blade.php
+++ resources/views/partials/content-relationship.blade.php
@@ -1,25 +1,25 @@
-<div class="max-w-7xl mx-auto px-4 sm:px-6">
-  <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-brand-darker sm:text-4xl sm:leading-10 mb-10">Related</h2> 
-
-  <?php
-    $tasks = get_posts(array(
-      'post_type' => array('post', 'archive', 'press'),
-      'meta_query' => array(
-        array(
-          'key' => 'people', // name of custom field
-          'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
-          'compare' => 'LIKE'
-        )
+<?php
+  $tasks = get_posts(array(
+    'post_type' => array('post', 'archive', 'press'),
+    'meta_query' => array(
+      array(
+        'key' => 'people',
+        'value' => '"' . get_the_ID() . '"', 
+        'compare' => 'LIKE'
       )
-    ));
-  ?>
+    )
+  ));
+?>
 
-  @if( $tasks )
-    <div class="w-full md:w-3/4 sm:-mx-5">
-      @php foreach( $tasks as $tasks ): @endphp
-      @include('partials.people.people-item')
-      @php endforeach; @endphp
-    </div>
-  @endif
-  
+@if( $tasks )
+<div class="max-w-7xl mx-auto px-4 sm:px-6 pt-20">
+  <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-brand-darker sm:text-4xl sm:leading-10 mb-10">
+    More from <?php the_title(); ?>
+  </h2> 
+  <div class="w-full md:w-3/4 sm:-mx-5">
+    @php foreach( $tasks as $tasks ): @endphp
+    @include('partials.people.people-item')
+    @php endforeach; @endphp
+  </div>
 </div>
+@endif
