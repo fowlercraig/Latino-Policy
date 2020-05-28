47355e8c0fd67c5a7bcebe765f474194600f988e
Craig Fowler <craig@howler.la>
2020-05-28 14:15:11 -0700

    people

--- resources/views/page-people.blade.php
+++ resources/views/page-people.blade.php
@@ -6,9 +6,13 @@
     @include('partials.page-header-people')
 
     <div class="md:space-y-20">
+      <div id="staff"></div>
       @include('partials.people.staff')
+      <div id="experts" class="h-px bg-gray-100"></div>
       @include('partials.people.experts')
+      <div id="fellows" class="h-px bg-gray-100"></div>
       @include('partials.people.fellows')
+      <div id="advisors" class="h-px bg-gray-100"></div>
       @include('partials.people.advisors')
     </div>
 
