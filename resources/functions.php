0cfc48ace3c416ccdb0061004b567bfd851b98bc
Craig Fowler <craig@howler.la>
2020-05-28 10:49:59 -0700

    removed post posttype

--- resources/functions.php
+++ resources/functions.php
@@ -148,3 +148,21 @@ function posts_link_attributes() {
   return 'class="inline-block mr-1 rounded-full inline-block text-sm text-white bg-brand-darker font-bold py-4 px-6 items-center relative group 
       hover:bg-brand-dark transition ease duration-300 hover:shadow-xl"';
 }
+
+// ************* Remove default Posts type since no blog *************
+// Remove side menu
+add_action( 'admin_menu', 'remove_default_post_type' );
+function remove_default_post_type() {
+    remove_menu_page( 'edit.php' );
+}
+// Remove +New post in top Admin Menu Bar
+add_action( 'admin_bar_menu', 'remove_default_post_type_menu_bar', 999 );
+function remove_default_post_type_menu_bar( $wp_admin_bar ) {
+    $wp_admin_bar->remove_node( 'new-post' );
+}
+// Remove Quick Draft Dashboard Widget
+add_action( 'wp_dashboard_setup', 'remove_draft_widget', 999 );
+function remove_draft_widget(){
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+}
+// End remove post type
