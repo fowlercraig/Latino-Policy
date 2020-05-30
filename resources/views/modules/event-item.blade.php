a63a0b976a0819a4678628935b7c97d340e9b290
Craig Fowler <craig@howler.la>
2020-05-30 12:53:49 -0700

    changes

--- resources/views/modules/event-item.blade.php
+++ resources/views/modules/event-item.blade.php
@@ -1,9 +1,9 @@
-<div>
+<a class="block group" href="@php echo $item['link']['url']; @endphp">
   <div class="space-y-2 lg:space-y-0 lg:grid grid-cols-5 gap-12 lg:items-center">
     <div class="col-span-3 lg:order-last">
-      <a href="" class="block">
-        <div class="aspect-ratio aspect-ratio--8x5 bg-gray-100"></div>
-      </a>
+      <div class="aspect-ratio aspect-ratio--8x5 bg-gray-100">
+        <?php the_post_thumbnail( 'large', array( 'class' => 'group-hover:scale-105 transform transition ease duration-1000 object-cover transition ease duration-300 w-full h-full' ) ); ?>
+      </div>
     </div>
     <div class="col-span-2 space-y-2">
       <h2 class="text-2xl leading-8 font-bold tracking-tight text-white sm:text-3xl sm:leading-9">
@@ -11,7 +11,7 @@
       </h2>
       <div class="text-brand-lighter">@php the_excerpt(); @endphp</div>
       <div class="h-px"></div>
-      <div><a href="@php echo $item['link']['url']; @endphp" class="{{ $learnmore }}">Learn More</a></div>
+      <div><p class="{{ $learnmore }}">Learn More</p></div>
     </div>
   </div>
-</div>
+</a>
