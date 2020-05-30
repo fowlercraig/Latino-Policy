172d936bc3c56928e16d1f6f1433cce6d5c2a92d
Craig Fowler <craig@howler.la>
2020-05-30 10:04:51 -0700

    images

--- resources/views/modules/news-item.blade.php
+++ resources/views/modules/news-item.blade.php
@@ -1,7 +1,5 @@
 <a href="@php the_permalink(); @endphp" title="@php the_title(); @endphp" class="sm:-m-6 sm:-mt-8 sm:p-6 flex flex-col space-y-2 justify-start rounded-lg sm:hover:bg-gray-50 transition ease-in-out duration-150">
-  <div class="aspect-ratio aspect-ratio--7x5 md:aspect-ratio--1x1 bg-gray-300">
-    <?php the_post_thumbnail( 'medium', array( 'class' => 'opacity-95 object-cover transition ease duration-300 w-full h-full' ) ); ?>
-  </div>
+  <?php the_post_thumbnail( 'medium', array( 'class' => 'opacity-95 object-cover transition ease duration-300 w-full h-full' ) ); ?>
   <div class="h-1"></div>
   <h5 class="text-lg leading-6 font-medium text-brand-dark mb-0">
     @if(get_the_terms(get_the_ID(), 'issue'))
