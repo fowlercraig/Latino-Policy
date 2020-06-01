985508d9d82a1a7476191ab95e048f99b45010ec
Craig Fowler <craig@howler.la>
2020-06-01 14:16:07 -0700

    slideshow

--- resources/views/home/slideshow.blade.php
+++ resources/views/home/slideshow.blade.php
@@ -9,26 +9,28 @@
         <img alt='@php echo $item["image"]["alt"]; @endphp' class="object-cover w-full h-full" src='@php echo $item["image"]["url"]; @endphp'>
       </div>
       <div class="container max-w-none relative z-10 order-last -mt-8">
-        <div class="max-w-lg xl:max-w-2xl space-y-4">
-          @if ($item['subtitle'])
-          <div class="inline-block bg-brand px-2 py-1">
-            <span class="text-white font-medium">@php echo $item['subtitle'] @endphp</span>
+        <div class="container max-w-none">
+          <div class="max-w-lg xl:max-w-2xl space-y-4">
+            @if ($item['subtitle'])
+            <div class="inline-block bg-brand px-2 py-1">
+              <span class="text-white font-medium">@php echo $item['subtitle'] @endphp</span>
+            </div>
+            @endif
+            @if ($item['title'])
+            <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-white sm:text-4xl xl:text-5xl sm:leading-10 xl:leading-13 mb-0">
+              @php echo $item['title'] @endphp
+            </h2>
+            @endif
+            @if ($item['description'])
+            <div class="text-white-75">@php echo $item['description'] @endphp</div>
+            @endif
+            @if ($item['link'])
+            <a
+              title="@php echo $item['title'] @endphp"
+              href="@php echo $item['link']['url'] @endphp" 
+              class="inline-block font-bold border-t-2 border-b-0 border-brand pt-1 text-white">@php echo $item['link']['title'] @endphp</a>
+            @endif
           </div>
-          @endif
-          @if ($item['title'])
-          <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-white sm:text-4xl xl:text-5xl sm:leading-10 xl:leading-13 mb-0">
-            @php echo $item['title'] @endphp
-          </h2>
-          @endif
-          @if ($item['description'])
-          <div class="text-white-75">@php echo $item['description'] @endphp</div>
-          @endif
-          @if ($item['link'])
-          <a
-            title="@php echo $item['title'] @endphp"
-            href="@php echo $item['link']['url'] @endphp" 
-            class="inline-block font-bold border-t-2 border-b-0 border-brand pt-1 text-white">@php echo $item['link']['title'] @endphp</a>
-          @endif
         </div>
       </div>
     </div>
