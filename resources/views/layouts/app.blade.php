2c99482682e41686a3118a688bef6ac1c5720178
Craig Fowler <craig@howler.la>
2020-05-31 19:55:39 -0700

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,22 +2,24 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class('text-gray-500') @endphp>
-    <a class="sr-only" href="#main" title="Skips to content">Skip to Content</a>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    @if (is_front_page())
-    <div class="wrap" role="document">
-    @else
-    <div class="wrap" role="document">
-    @endif
-      <div class="content">
-        <main id="main" class="main transition-fade">
-          @yield('content')
-        </main>
+    <div x-data="{ open: false }" @click.away="open = false">
+      <a class="sr-only" href="#main" title="Skips to content">Skip to Content</a>
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+      @if (is_front_page())
+      <div class="wrap" role="document">
+      @else
+      <div class="wrap" role="document">
+      @endif
+        <div class="content">
+          <main id="main" class="main transition-fade">
+            @yield('content')
+          </main>
+        </div>
       </div>
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
+      @php wp_footer() @endphp
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
   </body>
 </html>
