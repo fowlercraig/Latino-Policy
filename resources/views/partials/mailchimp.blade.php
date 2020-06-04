4df3efd1408ac9fdd1ebf1c58307e2f24e39b5fe
Craig Fowler <craig@howler.la>
2020-06-04 08:46:36 -0700

    mailchimp

---
+++ resources/views/partials/mailchimp.blade.php
@@ -0,0 +1,116 @@
+<style>
+    /* MailChimp Form Embed Code - Classic - 12/17/2015 v10.7 */
+  #mc_embed_signup form {display:block; position:relative; text-align:left; padding:10px 0 10px 3%}
+  #mc_embed_signup h2 {font-weight:bold; padding:0; margin:15px 0; font-size:1.4em;}
+  #mc_embed_signup input[type=checkbox]{-webkit-appearance:checkbox;}
+  #mc_embed_signup input[type=radio]{-webkit-appearance:radio;}
+  #mc_embed_signup input:focus {border-color:#333;}
+  #mc_embed_signup .small-meta {font-size: 11px;}
+  #mc_embed_signup .nowrap {white-space:nowrap;}
+
+  #mc_embed_signup .mc-field-group {clear:left; position:relative; width:96%; padding-bottom:3%; min-height:50px;}
+  #mc_embed_signup .size1of2 {clear:none; float:left; display:inline-block; width:46%; margin-right:4%;}
+  * html #mc_embed_signup .size1of2 {margin-right:2%; /* Fix for IE6 double margins. */}
+  #mc_embed_signup .mc-field-group label {display:block; margin-bottom:3px;}
+  #mc_embed_signup .mc-field-group input {display:block; width:100%; padding:8px 0; text-indent:2%;}
+  #mc_embed_signup .mc-field-group select {display:inline-block; width:99%; padding:5px 0; margin-bottom:2px;}
+
+  #mc_embed_signup .datefield, #mc_embed_signup .phonefield-us{padding:5px 0;}
+  #mc_embed_signup .datefield input, #mc_embed_signup .phonefield-us input{display:inline; width:60px; margin:0 2px; letter-spacing:1px; text-align:center; padding:5px 0 2px 0;}
+  #mc_embed_signup .phonefield-us .phonearea input, #mc_embed_signup .phonefield-us .phonedetail1 input{width:40px;}
+  #mc_embed_signup .datefield .monthfield input, #mc_embed_signup .datefield .dayfield input{width:30px;}
+  #mc_embed_signup .datefield label, #mc_embed_signup .phonefield-us label{display:none;}
+
+  #mc_embed_signup .indicates-required {text-align:right; font-size:11px; margin-right:4%;}
+  #mc_embed_signup .asterisk {color:#e85c41; font-size:150%; font-weight:normal; position:relative; top:5px;}     
+  #mc_embed_signup .clear {clear:both;}
+
+  #mc_embed_signup .mc-field-group.input-group ul {margin:0; padding:5px 0; list-style:none;}
+  #mc_embed_signup .mc-field-group.input-group ul li {display:block; padding:3px 0; margin:0;}
+  #mc_embed_signup .mc-field-group.input-group label {display:inline;}
+  #mc_embed_signup .mc-field-group.input-group input {display:inline; width:auto; border:none;}
+
+  #mc_embed_signup div#mce-responses {float:left; top:-1.4em; padding:0em .5em 0em .5em; overflow:hidden; width:90%; margin: 0 5%; clear: both;}
+  #mc_embed_signup div.response {margin:1em 0; padding:1em .5em .5em 0; font-weight:bold; float:left; top:-1.5em; z-index:1; width:80%;}
+  #mc_embed_signup #mce-error-response {display:none;}
+  #mc_embed_signup #mce-success-response {color:#529214; display:none;}
+  #mc_embed_signup label.error {display:block; float:none; width:auto; margin-left:1.05em; text-align:left; padding:.5em 0;}
+
+  #mc_embed_signup #num-subscribers {font-size:1.1em;}
+  #mc_embed_signup #num-subscribers span {padding:.5em; border:1px solid #ccc; margin-right:.5em; font-weight:bold;}
+
+  #mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error {display:inline-block; margin:2px 0 0 0; padding:5px 10px; background-color:rgba(255,255,255,0.85); -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; font-size:14px; font-weight:normal; z-index:1; color:#e85c41;}
+  #mc_embed_signup #mc-embedded-subscribe-form input.mce_inline_error {border:2px solid #e85c41;}
+</style>
+
+<div id="mc_embed_signup">
+<form 
+  style="padding:0" 
+  action="https://ucla.us16.list-manage.com/subscribe/post?u=d021c35e6a05ad46f78ac6f1d&amp;id=1d321a06c6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+  <div id="mc_embed_signup_scroll">
+    
+    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+    
+    <div class="mc-field-group">
+      <label class="sr-only" for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+      <input 
+        aria-label="Email Address" 
+        class="required appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" 
+        placeholder="Email Address"
+        type="text" 
+        value="" 
+        name="EMAIL" 
+        id="mce-EMAIL"
+      >
+    </div>
+    <div class="mc-field-group">
+      <label class="sr-only" for="mce-FNAME">First Name  <span class="asterisk">*</span></label>
+      <input 
+        aria-label="First Name" 
+        class="required appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" 
+        placeholder="First Name"
+        type="text" 
+        value="" 
+        name="FNAME" 
+        id="mce-FNAME"
+      >
+    </div>
+    <div class="mc-field-group">
+      <label class="sr-only" for="mce-LNAME">Last Name  <span class="asterisk">*</span></label>
+      <input 
+        aria-label="Last Name" 
+        class="required appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" 
+        placeholder="Last Name"
+        type="text" 
+        value="" 
+        name="LNAME" 
+        id="mce-LNAME" >
+    </div>
+    <div class="mc-field-group">
+      <label class="sr-only" for="mce-MMERGE3">Organization  <span class="asterisk">*</span></label>
+      <input 
+        aria-label="Organization" 
+        class="required appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" 
+        placeholder="Organization"
+        type="text" 
+        value="" 
+        name="MMERGE3" 
+        id="mce-MMERGE3"
+      >
+    </div>
+    
+    <div id="mce-responses" class="clear">
+      <div class="response" id="mce-error-response" style="display:none"></div>
+      <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>
+    
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d021c35e6a05ad46f78ac6f1d_1d321a06c6" tabindex="-1" value=""></div>
+    <div class="clear">
+      <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="cursor-pointer mt-3 w-full px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-700 focus:outline-none focus:shadow-outline active:bg-gray-900 transition duration-150 ease-in-out sm:mt-0 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
+    </div>
+
+  </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
