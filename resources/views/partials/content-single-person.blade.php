8563841c39b52fd66f1f181cb946835e2d73c687
Craig Fowler <craig@howler.la>
2020-06-05 11:12:31 -0700

    sticky

--- resources/views/partials/content-single-person.blade.php
+++ resources/views/partials/content-single-person.blade.php
@@ -4,7 +4,7 @@
     <div class="container space-y-4 lg:space-y-0 lg:grid grid-cols-4 xl:grid-cols-4 gap-12 sticky-container">
 
       <div class="col-span-2 xl:col-span-2 flex-none -ml-1 overflow-hidden lg:order-last">
-        <div class="sticky w-full" data-sticky-container='.sticky-container'>
+        <div class="sticky w-full" data-sticky-options='{"offset":100}' data-sticky-container='.sticky-container'>
           <?php the_post_thumbnail( 'large', array( 'class' => 'w-full' ) ); ?>
         </div>
       </div>
