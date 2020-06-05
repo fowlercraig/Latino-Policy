89f4396f697e6963866437d3d21b4b772ecbddc6
Craig Fowler <craig@howler.la>
2020-06-05 10:53:16 -0700

    added links

--- resources/views/partials/content-single-person.blade.php
+++ resources/views/partials/content-single-person.blade.php
@@ -8,45 +8,68 @@
       </div>
 
       <div class="col-span-2 xl:col-span-2 space-y-4">
-        <header class="py-4 flex border-b border-gray-200 items-center space-x-6">
+        <header class="space-y-3">
 
-          <div class="w-full">
-            <div class="text-xs sm:text-base font-medium rounded overflow-hidden">
-              @if(get_the_terms(get_the_ID(), 'role'))
-              <a href="/people/" title="Opens People Page" class="text-brand hover:underline">People</a>
-              <span class="text-gray-400">→</span>
-              @endif
+          <div class="text-sm sm:text-base font-medium rounded overflow-hidden">
+            @if(get_the_terms(get_the_ID(), 'role'))
+            <a href="/people/" title="Opens People Page" class="text-brand hover:underline">People</a>
+            <span class="text-gray-400">→</span>
+            @endif
+            @if(get_the_terms(get_the_ID(), 'role'))
+            <a title="Opens @php echo get_the_terms(get_the_ID(), 'role')[0]->name; @endphp Page" href="/people/@php echo get_the_terms(get_the_ID(), 'role')[0]->slug; @endphp" class="text-brand hover:underline">@php echo get_the_terms(get_the_ID(), 'role')[0]->name; @endphp</a>
+            @endif
+          </div>
 
-              @if(get_the_terms(get_the_ID(), 'role'))
-              <a title="Opens @php echo get_the_terms(get_the_ID(), 'role')[0]->name; @endphp Page" href="/people/@php echo get_the_terms(get_the_ID(), 'role')[0]->slug; @endphp" class="text-brand hover:underline">@php echo get_the_terms(get_the_ID(), 'role')[0]->name; @endphp</a>
-              @endif
-            </div>
-            <h1 class="font-display uppercase entry-title mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-7xl">{!! get_the_title() !!}</h1>
+          <h1 class="font-display uppercase entry-title mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-7xl">{!! get_the_title() !!}</h1>
+          <div class="border-t border-gray-200"></div>
 
-            <div class=""></div>
-
+          <div class="isssues">
             @if(is_singular( 'person' ) && get_the_terms(get_the_ID(), 'issue'))
-            <span class="text-brand">Issues</span>
+            <span class="font-medium text-brand">Issues</span>
             <span class="text-gray-400">→</span>
             @endif
 
             @if(get_the_terms(get_the_ID(), 'issue'))
-            <a href="@php echo $issue @endphp" class="text-brand hover:underline">@php echo get_the_terms(get_the_ID(), 'issue')[0]->name; @endphp</a>
-            @if (get_the_terms(get_the_ID(), 'issue')[1]->name)
-            <a href="@php echo $issue @endphp" class="text-brand hover:underline">@php echo get_the_terms(get_the_ID(), 'issue')[1]->name; @endphp</a>
+            <div class="font-medium issues inline-flex space-x-px">
+              @php $terms = get_the_terms(get_the_ID(), 'issue'); @endphp
+              @foreach ($terms as $term)
+              <a href="@php echo $term->slug; @endphp" class="text-brand hover:underline">@php echo $term->name; @endphp</a>
+              <span class="last:hidden">,</span>
+              @endforeach
+            </div>
             @endif
-            @if (get_the_terms(get_the_ID(), 'issue')[2]->name)
-            <a href="@php echo $issue @endphp" class="text-brand hover:underline">@php echo get_the_terms(get_the_ID(), 'issue')[2]->name; @endphp</a>
-            @endif
-            @endif
+          </div>
 
+          <div class="border-t border-gray-200"></div>
+
+          <div class="titles">
             @if(get_field('title'))
-            <div></div>
-            <span class="text-gray-500">@php the_field('title') @endphp</span>
+            <div class="font-medium">@php the_field('title') @endphp</div>
             @endif
 
-            @if(get_field('association'))
-            <span class="text-gray-500">@php the_field('association') @endphp</span>
+            @if(get_field('affiliation'))
+            <div class="font-medium">@php the_field('affiliation') @endphp</div>
+            @endif
+          </div>
+
+          <div class="border-t border-gray-200"></div>
+
+          <div class="titles inline-flex space-x-3 items-center">
+            @php $email = get_field('email') @endphp
+            @if($email)
+            <a href="@php echo $email['url'] @endphp" class="text-gray-900 hover:text-brand transition ease duration-150">
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/>
+              </svg>
+            </a>
+            @endif
+
+            @if(get_field('twitter'))
+            <a href="@php echo $email['url'] @endphp" class="text-gray-900 hover:text-brand transition ease duration-150">
+              <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
+              </svg>
+            </a>
             @endif
           </div>
 
