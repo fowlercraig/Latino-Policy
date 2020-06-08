72622d32a014613ed9c56dbd638374cbf93904f0
Craig Fowler <craig@howler.la>
2020-06-08 08:41:58 -0700

    hello

---
+++ resources/views/vrp/contact.blade.php
@@ -0,0 +1,33 @@
+
+@php $contact = get_field('contact_us') @endphp
+
+<section id="modules">
+  <div class="space-y-20 lg:space-y-20">
+    <div data-aos="fade-up">
+      <div class="space-y-2 lg:space-y-0 lg:grid grid-cols-5 gap-12 lg:items-center">
+        <div class="col-span-3 lg:order-last">
+          @php $image = $contact['image']; $size = 'large'; @endphp
+          <div class="bg-gray-100">
+            image
+            @if (wp_get_attachment_caption($image))
+              <span class="pt-2 pr-4 bg-white text-xs font-medium inline-block text-brand-dark absolute bottom-0">
+                @php echo wp_get_attachment_caption($image); @endphp
+              </span>
+            @endif
+            @php echo wp_get_attachment_image( $image, $size ); @endphp
+          </div>
+        </div>
+        <div class="col-span-2 space-y-2">
+          <h2 class="text-2xl sm:text-3xl xl:text-5xl leading-9 font-extrabold tracking-tight text-brand-dark sm:text-4xl sm:leading-10 xl:leading-13">
+            @php echo $contact['title'] @endphp
+          </h2>
+          <div>@php echo $contact['description'] @endphp</div>
+          @if ($contact['link'])
+          <div class="h-px"></div>
+          <div><a href="@php echo $contact['link']['url']; @endphp" class="{{ $learnmore }}">Learn More</a></div>
+          @endif
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ resources/views/vrp/experts.blade.php
@@ -0,0 +1,43 @@
+@php
+  $link = get_field('experts_link');
+  $ids = get_field('experts', false, false);
+  $args = array(
+    'post_type'      => 'person',
+    'posts_per_page' => 8,
+    'order'          => 'ASC',
+    'post__in'       => $ids,
+    'post_status'    => 'any',
+    'orderby'        => 'post__in',
+  );
+  $parent = new WP_Query( $args );
+  if ( $parent->have_posts() ):
+@endphp
+
+<section id="experts">
+  <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-8 lg:gap-12">
+    <header class="col-span-2 lg:col-span-4 grid grid-cols-2 gap-4 sm:gap-8 lg:gap-12">
+      <div>
+        <h2 class="text-2xl sm:text-3xl xl:text-5xl leading-9 font-extrabold tracking-tight text-brand-dark sm:text-4xl sm:leading-10 xl:leading-13">
+          @php the_field('experts_headline') @endphp
+        </h2> 
+        @if($link)
+        <div class="h-1"></div>
+        <a class="inline-block font-bold border-b-2 border-brand pb-1 text-brand transition ease duration-200 hover:border-brand-light" href="@php echo $link['url'] @endphp">
+          @php echo $link['title'] @endphp
+        </a>
+        @endif
+      </div>
+      <div>
+        @php the_field('description'); @endphp
+      </div>
+    </header>
+
+    @php while ( $parent->have_posts() ) : $parent->the_post(); @endphp
+    @include('people.person')
+    @php endwhile; @endphp
+
+  </div>
+</section>
+
+@endif
+@php wp_reset_postdata() @endphp
