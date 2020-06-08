ab35238dd611cfbafc65e2a03fc21036e6b9174e
Craig Fowler <craig@howler.la>
2020-06-08 07:30:37 -0700

    hello

---
+++ resources/views/newsroom/press-archive.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+  <a href="/press-archive" class="flex items-center h-64 justify-center bg-gray-100 relative overflow-hidden group hover:bg-brand-dark transition duration-300">
+    <div class="text-center space-y-2 group-hover:text-white transition duration-300">
+      <p>For more, visit the</p>
+      <div class="text-3xl leading-9 font-extrabold text-brand-dark font-display uppercase sm:text-4xl xl:text-6xl sm:leading-10 xl:leading-13 mb-0 group-hover:text-white transition duration-300">
+        Press Archive
+      </div>
+      <div></div>
+      <span class="inline-block border-t-2 border-brand-dark py-1 font-medium text-brand-dark transition duration-300 group-hover:text-white group-hover:border-white">Learn More</span>
+    </div>
+    <div class="opacity-25 absolute top-0 left-0 pointer-events-none z-0">
+      <img class="w-1/2 opacity-50" alt="Pattern" src="@asset('images/halftone.png')">
+    </div>
+  </a>
+</div>
