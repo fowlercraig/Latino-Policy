8a5463ac9b118a33dd58c35ee0d77fe31680fd9c
Craig Fowler <craig@howler.la>
2020-06-11 10:11:46 -0700

    fixed

--- resources/views/vrp/voting.blade.php
+++ resources/views/vrp/voting.blade.php
@@ -2,11 +2,13 @@
 <div class="bg-black" data-aos="fade-up">
   <div class="sm:grid grid-cols-2 items-center">
     <div class="aspect-ratio aspect-ratio--16x9 sm:aspect-ratio--1x1 md:aspect-ratio--7x5 bg-gray-900 sm:order-last">
-      @php $alt = get_post_meta($page, '_wp_attachment_image_alt', true); @endphp
-      <img 
-        class="object-cover w-full h-full object-top"
-        alt="@php echo $alt @endphp"
-        src="<?php echo get_the_post_thumbnail_url($page,'full'); ?>" />
+      <div class="absolute inset-x-0">
+        @php $alt = get_post_meta($page, '_wp_attachment_image_alt', true); @endphp
+        <img 
+          class="object-cover w-full h-full object-top"
+          alt="@php echo $alt @endphp"
+          src="<?php echo get_the_post_thumbnail_url($page,'full'); ?>" />
+        </div>
     </div>
     <div class="space-y-4 p-4 sm:p-8">
       <h2 class="text-white font-display uppercase text-5xl lg:text-6xl leading-12 lg:leading-15">@php echo get_the_title($page); @endphp</h2>
