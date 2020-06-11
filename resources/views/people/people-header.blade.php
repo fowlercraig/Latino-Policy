176552f73fd5354694f742df5937f3cd41a1c4d2
Craig Fowler <craig@howler.la>
2020-06-11 10:25:00 -0700

    people header

--- resources/views/people/people-header.blade.php
+++ resources/views/people/people-header.blade.php
@@ -1,11 +1,26 @@
+@php
+  $args = array(
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post_parent'    => 630,
+    'order'          => 'ASC',
+    'orderby'        => 'menu_order'
+  );
+  $parent = new WP_Query( $args );
+  if ( $parent->have_posts() ):
+@endphp
+
 <div class="bg-brand">
   <div class="container py-3 font-medium space-x-6">
     @php $member_group_terms = get_terms( 'role' ); @endphp
-    @foreach ( $member_group_terms as $member_group_term )
+    @php while ( $parent->have_posts() ) : $parent->the_post() @endphp
     <a 
       class="py-1 text-white inline-block transition duration-300 ease hover:text-white-75" 
       title="Opens @php echo $member_group_term->name; @endphp page in same window" 
-      href="/people/@php echo $member_group_term->name; @endphp">@php echo $member_group_term->name; @endphp</a>
-    @endforeach
+      href="@php the_permalink() @endphp">@php the_title() @endphp</a>
+    @php endwhile @endphp
   </div>
 </div>
+
+@endif
+@php wp_reset_postdata() @endphp
