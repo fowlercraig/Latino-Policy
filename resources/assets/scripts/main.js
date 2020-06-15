f9e6e04aea9b220fec9eb4f48a806b3883a2bf84
Craig Fowler <craig@howler.la>
2020-06-15 14:52:11 -0700

    removed

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -9,7 +9,7 @@ import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 //import aboutUs from './routes/about';
-import people from './routes/people';
+//import people from './routes/people';
 import fellows from './routes/fellows';
 
 /** Populate Router instance with DOM routes */
@@ -19,7 +19,7 @@ const routes = new Router({
   // Home page
   home,
   // About Us page, note the change from about-us to aboutUs.
-  people,
+  //people,
   fellows,
 });
 
