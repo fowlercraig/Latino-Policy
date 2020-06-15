f84137940114792cfa4c31509a2dfe4f7744dce8
Craig Fowler <craig@howler.la>
2020-06-15 14:47:31 -0700

    added history

--- resources/views/about/history.blade.php
+++ resources/views/about/history.blade.php
@@ -7,7 +7,8 @@
           @php echo wp_get_attachment_image( $image, $size ); @endphp
         </div>
         <div class="space-y-2 lg:order-last">
-          <h2 class="text-2xl sm:text-3xl leading-9 font-bold tracking-tight text-brand-dark sm:text-4xl sm:leading-10">
+          <span class="inline-block font-bold text-brand-dark relative group">History</span>
+          <h2 class="text-2xl sm:text-3xl leading-9 font-bold tracking-tight text-brand-darker sm:text-4xl sm:leading-10">
             @php the_field('title') @endphp
           </h2>
           <div class="h-2"></div>
