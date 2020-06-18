4f1c6968a6832241180c4c8c672dd1c6d70f5b52
Craig Fowler <craig@howler.la>
2020-06-18 16:43:53 -0700

    stuff

--- resources/views/newsroom/press.blade.php
+++ resources/views/newsroom/press.blade.php
@@ -1,15 +1,15 @@
 @php $image = get_field('press_image'); @endphp
 @php $link  = get_field('press_link'); @endphp
 <div class="container">
-  <section id="press-release" class="bg-brand-darker">
-    <div class="sm:grid grid-cols-5 items-center">
+  <section id="press-release" class="bg-brand-darker relative">
+    <div class="sm:grid grid-cols-5 items-center relative z-10">
       <div class="col-span-3 aspect-ratio aspect-ratio--16x9 sm:aspect-ratio--1x1 md:aspect-ratio--16x9 bg-gray-900 sm:order-last relative">
         <div class="absolute inset-0">
           @php echo wp_get_attachment_image( $image, $size, '', array( 'class' => 'object-cover transition ease duration-300 w-full h-full' ) ); @endphp
         </div>
       </div>
       <div class="col-span-2 space-y-4 p-8">
-        <h2 class="text-2xl sm:text-3xl xl:text-5xl leading-9 font-bold tracking-tight text-white sm:text-4xl sm:leading-10 xl:leading-13">@php the_field('press_title') @endphp</h2>
+        <h2 class="font-display uppercase text-3xl sm:text-3xl lg:text-7xl  leading-7 sm:leading-9 font-bold text-white sm:text-4xl sm:leading-10 lg:leading-16 lg:tracking-tight">@php the_field('press_title') @endphp</h2>
         <div class="text-gray-300">@php the_field('press_description') @endphp</div>
         <div>
           <a
@@ -19,6 +19,9 @@
           >@php echo $link['title'] @endphp</a>
         </div>
       </div>
+    </div>
+    <div class="opacity-25 absolute top-0 left-0 pointer-events-none z-0">
+      <img class="w-1/2" alt="Pattern" src="@asset('images/halftone.png')">
     </div>
   </section>
 </div>
