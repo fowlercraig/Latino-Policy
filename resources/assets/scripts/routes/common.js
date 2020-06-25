624ed7153c391912e6f55c7a210272f4fbd04daf
Craig Fowler <craig@howler.la>
2020-06-25 11:33:23 -0700

    filters

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -23,6 +23,43 @@ export default {
   finalize() {
     function build() {
 
+      function addClass(el, klass) {
+        el.classList.add(klass);
+      }
+
+      function removeClass(el, klass) {
+        el.classList.remove(klass);
+      }
+
+      const accordionItems = document.querySelectorAll('.alm-filter');
+      const accordionContentPanes = document.querySelectorAll('.alm-filter ul');
+
+      // Hide each besides the targeted accordion on click
+      accordionItems.forEach(function(accordion) {
+        // Clicked accordeions clickable target
+        const accordionTitleRow = accordion.querySelector('.alm-filter--title');
+        accordion.querySelector('ul').classList.add('hidden');
+        accordionTitleRow.addEventListener('click', toggleAccordion);
+        // console.log(accordion.firstElementChild);  
+      });
+
+      // Show first by default
+      accordionItems[0].querySelector('.alm-filter ul').classList.remove('hidden');
+
+      function toggleAccordion(e) {
+        accordionContentPanes.forEach(function(content) {
+          // Check if clicked row matches the content's previous element sibling
+          console.log(content); 
+          if (content.previousElementSibling === e.target) {
+            removeClass(content, 'hidden'); 
+            addClass(content.parentElement, 'active');
+          } else {
+            removeClass(content.parentElement, 'active');
+            addClass(content, 'hidden');
+          }
+        });
+      }
+
       feather.replace();
       new Rellax('.rellax');
       $('.carousel').carousel();  
