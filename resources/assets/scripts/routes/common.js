cb5e3938d8d7c0fbe32099a4450b4e2fed43b9e2
Craig Fowler <craig@howler.la>
2020-06-25 11:49:51 -0700

    things

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -23,42 +23,28 @@ export default {
   finalize() {
     function build() {
 
-      function addClass(el, klass) {
-        el.classList.add(klass);
-      }
+      //const accordionItems = document.querySelectorAll('.alm-filter');
+      //const accordionContentPanes = document.querySelectorAll('.alm-filter ul');
 
-      function removeClass(el, klass) {
-        el.classList.remove(klass);
-      }
+      /*eslint-disable */
+      (function($) {
+      var allPanels = $('.alm-filter ul').hide();
+        
+      $('.alm-filter .alm-filter--title').click(function() {
+          var $this = $(this);
+          var $target =  $this.next();
 
-      const accordionItems = document.querySelectorAll('.alm-filter');
-      const accordionContentPanes = document.querySelectorAll('.alm-filter ul');
+          console.log($this);
 
-      // Hide each besides the targeted accordion on click
-      accordionItems.forEach(function(accordion) {
-        // Clicked accordeions clickable target
-        const accordionTitleRow = accordion.querySelector('.alm-filter--title');
-        accordion.querySelector('ul').classList.add('hidden');
-        accordionTitleRow.addEventListener('click', toggleAccordion);
-        // console.log(accordion.firstElementChild);  
-      });
-
-      // Show first by default
-      accordionItems[0].querySelector('.alm-filter ul').classList.remove('hidden');
-
-      function toggleAccordion(e) {
-        accordionContentPanes.forEach(function(content) {
-          // Check if clicked row matches the content's previous element sibling
-          console.log(content); 
-          if (content.previousElementSibling === e.target) {
-            removeClass(content, 'hidden'); 
-            addClass(content.parentElement, 'active');
-          } else {
-            removeClass(content.parentElement, 'active');
-            addClass(content, 'hidden');
+          if(!$target.hasClass('active')){
+             allPanels.removeClass('active').hide();
+             $target.addClass('active').show();
           }
-        });
-      }
+          
+        return false;
+      });
+      })(jQuery);
+      /*eslint-enable */
 
       feather.replace();
       new Rellax('.rellax');
