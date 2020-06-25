074685e6d782e8e997bb540545195b0c8744118e
Craig Fowler <craig@howler.la>
2020-06-25 13:32:39 -0700

    voting rights

---
+++ resources/views/page-voting-rights-project.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    <div class="space-y-20 container">
+      <div class="h-px bg-gray-100"></div>
+      @include('modules.modules')
+      @include('modules.donate')
+    </div>
+  @endwhile
+@endsection
