48aa4cfb35fb55825ed50b4ca23baab95a1128f5
Craig Fowler <craig@howler.la>
2021-02-26 18:35:12 -0800

    fix

--- resources/views/components/entry-contributors.blade.php
+++ resources/views/components/entry-contributors.blade.php
@@ -3,7 +3,7 @@
 @if($contributors)
   <div class="grid grid-cols-2 gap-2">
     @foreach( $contributors as $contributor )
-      <a href="@sub('download','url')" class="flex items-center">
+      <a href="@permalink($contributor)" class="flex items-center">
         @set($image, get_post_thumbnail_id($contributor))
         @image($image, 'thumbnail', ['alt' => 'My alt tag', 'class' => 'my-0 mr-4 w-10 h-10 rounded-full'])
         <div class="leading-tight text-sm">
