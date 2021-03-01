ee36faa7abb49801b31ad9df2737a105dce7531f
Craig Fowler <craig@howler.la>
2021-03-01 13:45:22 -0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: {
     content: [
+      './*.php',
       './app/**/*.php',
       './resources/**/*.php',
       './resources/**/*.vue',
