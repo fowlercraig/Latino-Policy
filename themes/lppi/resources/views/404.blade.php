6be8e09648365a286cd01361eaa91f3bb730e10a
Craig Fowler <craig@howler.la>
2021-06-03 17:11:07 -0700

    lppi theme

--- themes/lppi/resources/views/404.blade.php
+++ themes/lppi/resources/views/404.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  @include('partials.page-header-404')
   <div class="container">
     <div class="prose">
       @if (! have_posts())
-        <x-alert type="warning">
+        <x-alert type="warning" class="rounded">
           {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
         </x-alert>
-
-        {!! get_search_form(false) !!}
       @endif
     </div>
   </div>
